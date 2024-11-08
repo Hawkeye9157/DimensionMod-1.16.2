4248b3afa4a605e305a9f093d6096f38a1181ea8
KayDesist <160885770+KayDesist@users.noreply.github.com>
2024-11-08 16:23:51 -0700

    Add files via upload

--- Kay_EntityItems/Entities/vinespirit.java
+++ Kay_EntityItems/Entities/vinespirit.java
@@ -6,34 +6,27 @@
 public class k.mod.vinespirit<T extends Entity> extends EntityModel<T> {
 	// This layer location should be baked with EntityRendererProvider.Context in the entity renderer and passed into this model's constructor
 	public static final ModelLayerLocation LAYER_LOCATION = new ModelLayerLocation(new ResourceLocation("modid", "k.mod.vinespirit"), "main");
+	private final ModelPart Wig;
 	private final ModelPart Head;
 	private final ModelPart Body;
-	private final ModelPart Arms;
-	private final ModelPart Legs;
-	private final ModelPart Wig;
+	private final ModelPart L_Arm;
+	private final ModelPart R_Arm;
+	private final ModelPart L_Leg;
+	private final ModelPart R_Leg;
 
 	public k.mod.vinespirit(ModelPart root) {
+		this.Wig = root.getChild("Wig");
 		this.Head = root.getChild("Head");
 		this.Body = root.getChild("Body");
-		this.Arms = root.getChild("Arms");
-		this.Legs = root.getChild("Legs");
-		this.Wig = root.getChild("Wig");
+		this.L_Arm = root.getChild("L_Arm");
+		this.R_Arm = root.getChild("R_Arm");
+		this.L_Leg = root.getChild("L_Leg");
+		this.R_Leg = root.getChild("R_Leg");
 	}
 
 	public static LayerDefinition createBodyLayer() {
 		MeshDefinition meshdefinition = new MeshDefinition();
 		PartDefinition partdefinition = meshdefinition.getRoot();
-
-		PartDefinition Head = partdefinition.addOrReplaceChild("Head", CubeListBuilder.create().texOffs(0, 13).addBox(-3.0F, -10.0F, -3.0F, 5.0F, 5.0F, 6.0F, new CubeDeformation(0.0F))
-		.texOffs(26, 5).addBox(1.0F, -5.0F, -1.0F, 1.0F, 1.0F, 2.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 24.0F, 0.0F));
-
-		PartDefinition Body = partdefinition.addOrReplaceChild("Body", CubeListBuilder.create().texOffs(0, 24).addBox(-4.0F, -2.0F, -2.0F, 3.0F, 3.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offset(2.0F, 21.0F, 0.0F));
-
-		PartDefinition Arms = partdefinition.addOrReplaceChild("Arms", CubeListBuilder.create().texOffs(22, 31).addBox(-1.0F, -5.0F, -3.0F, 1.0F, 2.0F, 1.0F, new CubeDeformation(0.0F))
-		.texOffs(26, 31).addBox(-1.0F, -5.0F, 2.0F, 1.0F, 2.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 24.0F, 0.0F));
-
-		PartDefinition Legs = partdefinition.addOrReplaceChild("Legs", CubeListBuilder.create().texOffs(26, 11).addBox(-1.0F, -2.0F, -2.0F, 1.0F, 1.0F, 1.0F, new CubeDeformation(0.0F))
-		.texOffs(30, 11).addBox(-1.0F, -2.0F, 1.0F, 1.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 24.0F, 0.0F));
 
 		PartDefinition Wig = partdefinition.addOrReplaceChild("Wig", CubeListBuilder.create().texOffs(26, 0).addBox(-1.0F, -12.0F, -1.0F, 4.0F, 2.0F, 3.0F, new CubeDeformation(0.0F))
 		.texOffs(26, 8).addBox(2.0F, -10.0F, 0.0F, 1.0F, 1.0F, 2.0F, new CubeDeformation(0.0F))
@@ -46,6 +39,19 @@ public class k.mod.vinespirit<T extends Entity> extends EntityModel<T> {
 		.texOffs(22, 13).addBox(-6.0F, -9.0F, -3.0F, 2.0F, 4.0F, 6.0F, new CubeDeformation(0.0F))
 		.texOffs(0, 0).addBox(-4.0F, -11.0F, -4.0F, 5.0F, 5.0F, 8.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 24.0F, 0.0F));
 
+		PartDefinition Head = partdefinition.addOrReplaceChild("Head", CubeListBuilder.create().texOffs(0, 13).addBox(-3.0F, -10.0F, -3.0F, 5.0F, 5.0F, 6.0F, new CubeDeformation(0.0F))
+		.texOffs(26, 5).addBox(1.0F, -5.0F, -1.0F, 1.0F, 1.0F, 2.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 24.0F, 0.0F));
+
+		PartDefinition Body = partdefinition.addOrReplaceChild("Body", CubeListBuilder.create().texOffs(0, 24).addBox(-4.0F, -2.0F, -2.0F, 3.0F, 3.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offset(2.0F, 21.0F, 0.0F));
+
+		PartDefinition L_Arm = partdefinition.addOrReplaceChild("L_Arm", CubeListBuilder.create().texOffs(26, 31).addBox(-1.0F, -5.0F, 2.0F, 1.0F, 2.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 24.0F, 0.0F));
+
+		PartDefinition R_Arm = partdefinition.addOrReplaceChild("R_Arm", CubeListBuilder.create().texOffs(22, 31).addBox(-1.0F, -5.0F, -3.0F, 1.0F, 2.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 24.0F, 0.0F));
+
+		PartDefinition L_Leg = partdefinition.addOrReplaceChild("L_Leg", CubeListBuilder.create().texOffs(30, 11).addBox(-1.0F, -2.0F, 1.0F, 1.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 24.0F, 0.0F));
+
+		PartDefinition R_Leg = partdefinition.addOrReplaceChild("R_Leg", CubeListBuilder.create().texOffs(26, 11).addBox(-1.0F, -2.0F, -2.0F, 1.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 24.0F, 0.0F));
+
 		return LayerDefinition.create(meshdefinition, 64, 64);
 	}
 
@@ -56,10 +62,12 @@ public class k.mod.vinespirit<T extends Entity> extends EntityModel<T> {
 
 	@Override
 	public void renderToBuffer(PoseStack poseStack, VertexConsumer vertexConsumer, int packedLight, int packedOverlay, float red, float green, float blue, float alpha) {
+		Wig.render(poseStack, vertexConsumer, packedLight, packedOverlay, red, green, blue, alpha);
 		Head.render(poseStack, vertexConsumer, packedLight, packedOverlay, red, green, blue, alpha);
 		Body.render(poseStack, vertexConsumer, packedLight, packedOverlay, red, green, blue, alpha);
-		Arms.render(poseStack, vertexConsumer, packedLight, packedOverlay, red, green, blue, alpha);
-		Legs.render(poseStack, vertexConsumer, packedLight, packedOverlay, red, green, blue, alpha);
-		Wig.render(poseStack, vertexConsumer, packedLight, packedOverlay, red, green, blue, alpha);
+		L_Arm.render(poseStack, vertexConsumer, packedLight, packedOverlay, red, green, blue, alpha);
+		R_Arm.render(poseStack, vertexConsumer, packedLight, packedOverlay, red, green, blue, alpha);
+		L_Leg.render(poseStack, vertexConsumer, packedLight, packedOverlay, red, green, blue, alpha);
+		R_Leg.render(poseStack, vertexConsumer, packedLight, packedOverlay, red, green, blue, alpha);
 	}
 }
