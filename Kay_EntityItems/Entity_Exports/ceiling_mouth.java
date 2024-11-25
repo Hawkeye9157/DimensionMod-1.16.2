fd520d60bc9147d52eb8abce705dce403ebb6d57
KayDesist <160885770+KayDesist@users.noreply.github.com>
2024-11-25 16:24:02 -0700

    animations for spider and the screamer

--- Kay_EntityItems/Entity_Exports/ceiling_mouth.java
+++ Kay_EntityItems/Entity_Exports/ceiling_mouth.java
@@ -7,21 +7,43 @@ public class ceiling_mouth extends EntityModel<Entity> {
 	private final ModelRenderer Mouth;
 	private final ModelRenderer Lips;
 	private final ModelRenderer Teeth;
+	private final ModelRenderer topRow;
+	private final ModelRenderer btmRow;
 	private final ModelRenderer Base;
-	private final ModelRenderer Tentacles;
-	private final ModelRenderer TopL_Tentacle;
+	private final ModelRenderer TopTentacle;
+	private final ModelRenderer baseTT;
 	private final ModelRenderer cube_r1;
-	private final ModelRenderer TopR_Tentacle;
+	private final ModelRenderer middleTT;
+	private final ModelRenderer tipTT;
+	private final ModelRenderer Btm_L_Tentacle;
+	private final ModelRenderer baseBLT;
 	private final ModelRenderer cube_r2;
-	private final ModelRenderer LeftT_Tentacle;
+	private final ModelRenderer middleBLT;
+	private final ModelRenderer tipBLT;
+	private final ModelRenderer Btm_R_Tentacle;
+	private final ModelRenderer baseBRT;
 	private final ModelRenderer cube_r3;
-	private final ModelRenderer LeftB_Tentacle;
+	private final ModelRenderer middleBRT;
+	private final ModelRenderer tipBRT;
+	private final ModelRenderer L_Top_Tentacle;
+	private final ModelRenderer baseLTT;
+	private final ModelRenderer middleLTT;
+	private final ModelRenderer tipLTT;
 	private final ModelRenderer cube_r4;
-	private final ModelRenderer RightT_Tentacle;
+	private final ModelRenderer L_Btm_Tentacle;
+	private final ModelRenderer baseLBT;
+	private final ModelRenderer middleLBT;
+	private final ModelRenderer tipLBT;
 	private final ModelRenderer cube_r5;
-	private final ModelRenderer RightB_Tentacle;
+	private final ModelRenderer R_Top_Tentacle;
+	private final ModelRenderer baseRTT;
+	private final ModelRenderer middleRTT;
+	private final ModelRenderer tipRTT;
 	private final ModelRenderer cube_r6;
-	private final ModelRenderer Bottom_Tentacle;
+	private final ModelRenderer R_Btm_Tentacle;
+	private final ModelRenderer baseRBT;
+	private final ModelRenderer middleRBT;
+	private final ModelRenderer tipRBT;
 	private final ModelRenderer cube_r7;
 
 	public ceiling_mouth() {
@@ -36,129 +58,224 @@ public class ceiling_mouth extends EntityModel<Entity> {
 		Lips = new ModelRenderer(this);
 		Lips.setPos(0.0F, 0.0F, 0.0F);
 		Mouth.addChild(Lips);
-		Lips.texOffs(0, 54).addBox(0.0F, -5.0F, -4.0F, 5.0F, 3.0F, 2.0F, 0.0F, false);
-		Lips.texOffs(28, 62).addBox(-3.0F, -4.0F, -5.0F, 3.0F, 2.0F, 1.0F, 0.0F, false);
-		Lips.texOffs(20, 62).addBox(0.0F, -4.0F, -5.0F, 3.0F, 2.0F, 1.0F, 0.0F, false);
-		Lips.texOffs(54, 62).addBox(-11.0F, -5.0F, -1.0F, 2.0F, 3.0F, 1.0F, 0.0F, false);
-		Lips.texOffs(48, 62).addBox(9.0F, -5.0F, -1.0F, 2.0F, 3.0F, 1.0F, 0.0F, false);
-		Lips.texOffs(42, 62).addBox(7.0F, -5.0F, -1.0F, 2.0F, 3.0F, 1.0F, 0.0F, false);
-		Lips.texOffs(20, 65).addBox(11.0F, -4.0F, -2.0F, 1.0F, 2.0F, 1.0F, 0.0F, false);
-		Lips.texOffs(24, 59).addBox(-12.0F, -4.0F, -2.0F, 1.0F, 2.0F, 1.0F, 0.0F, false);
-		Lips.texOffs(8, 65).addBox(-9.0F, -5.0F, 0.0F, 1.0F, 3.0F, 1.0F, 0.0F, false);
-		Lips.texOffs(4, 65).addBox(8.0F, -5.0F, 0.0F, 1.0F, 3.0F, 1.0F, 0.0F, false);
-		Lips.texOffs(0, 65).addBox(7.0F, -5.0F, 1.0F, 1.0F, 3.0F, 1.0F, 0.0F, false);
-		Lips.texOffs(64, 56).addBox(-8.0F, -5.0F, 1.0F, 1.0F, 3.0F, 1.0F, 0.0F, false);
-		Lips.texOffs(36, 62).addBox(-9.0F, -5.0F, -1.0F, 2.0F, 3.0F, 1.0F, 0.0F, false);
-		Lips.texOffs(12, 62).addBox(4.0F, -5.0F, -2.0F, 3.0F, 3.0F, 1.0F, 0.0F, false);
-		Lips.texOffs(60, 29).addBox(-7.0F, -5.0F, -2.0F, 3.0F, 3.0F, 1.0F, 0.0F, false);
-		Lips.texOffs(60, 25).addBox(-7.0F, -5.0F, 2.0F, 3.0F, 3.0F, 1.0F, 0.0F, false);
-		Lips.texOffs(60, 21).addBox(4.0F, -5.0F, 2.0F, 3.0F, 3.0F, 1.0F, 0.0F, false);
-		Lips.texOffs(0, 18).addBox(-4.0F, -5.0F, 2.0F, 8.0F, 3.0F, 3.0F, 0.0F, false);
-		Lips.texOffs(14, 54).addBox(-5.0F, -5.0F, -4.0F, 5.0F, 3.0F, 2.0F, 0.0F, false);
+		Lips.texOffs(0, 54).addBox(0.0F, -9.0F, -4.0F, 5.0F, 3.0F, 2.0F, 0.0F, false);
+		Lips.texOffs(28, 62).addBox(-3.0F, -8.0F, -5.0F, 3.0F, 2.0F, 1.0F, 0.0F, false);
+		Lips.texOffs(20, 62).addBox(0.0F, -8.0F, -5.0F, 3.0F, 2.0F, 1.0F, 0.0F, false);
+		Lips.texOffs(54, 62).addBox(-11.0F, -9.0F, -1.0F, 2.0F, 3.0F, 1.0F, 0.0F, false);
+		Lips.texOffs(48, 62).addBox(9.0F, -9.0F, -1.0F, 2.0F, 3.0F, 1.0F, 0.0F, false);
+		Lips.texOffs(42, 62).addBox(7.0F, -9.0F, -1.0F, 2.0F, 3.0F, 1.0F, 0.0F, false);
+		Lips.texOffs(20, 65).addBox(11.0F, -8.0F, -2.0F, 1.0F, 2.0F, 1.0F, 0.0F, false);
+		Lips.texOffs(24, 59).addBox(-12.0F, -8.0F, -2.0F, 1.0F, 2.0F, 1.0F, 0.0F, false);
+		Lips.texOffs(8, 65).addBox(-9.0F, -9.0F, 0.0F, 1.0F, 3.0F, 1.0F, 0.0F, false);
+		Lips.texOffs(4, 65).addBox(8.0F, -9.0F, 0.0F, 1.0F, 3.0F, 1.0F, 0.0F, false);
+		Lips.texOffs(0, 65).addBox(7.0F, -9.0F, 1.0F, 1.0F, 3.0F, 1.0F, 0.0F, false);
+		Lips.texOffs(64, 56).addBox(-8.0F, -9.0F, 1.0F, 1.0F, 3.0F, 1.0F, 0.0F, false);
+		Lips.texOffs(36, 62).addBox(-9.0F, -9.0F, -1.0F, 2.0F, 3.0F, 1.0F, 0.0F, false);
+		Lips.texOffs(12, 62).addBox(4.0F, -9.0F, -2.0F, 3.0F, 3.0F, 1.0F, 0.0F, false);
+		Lips.texOffs(60, 29).addBox(-7.0F, -9.0F, -2.0F, 3.0F, 3.0F, 1.0F, 0.0F, false);
+		Lips.texOffs(60, 25).addBox(-7.0F, -9.0F, 2.0F, 3.0F, 3.0F, 1.0F, 0.0F, false);
+		Lips.texOffs(60, 21).addBox(4.0F, -9.0F, 2.0F, 3.0F, 3.0F, 1.0F, 0.0F, false);
+		Lips.texOffs(0, 18).addBox(-4.0F, -9.0F, 2.0F, 8.0F, 3.0F, 3.0F, 0.0F, false);
+		Lips.texOffs(14, 54).addBox(-5.0F, -9.0F, -4.0F, 5.0F, 3.0F, 2.0F, 0.0F, false);
 
 		Teeth = new ModelRenderer(this);
 		Teeth.setPos(0.0F, 0.0F, 0.0F);
 		Mouth.addChild(Teeth);
-		Teeth.texOffs(12, 59).addBox(0.0F, -4.0F, -2.0F, 4.0F, 1.0F, 2.0F, 0.0F, false);
-		Teeth.texOffs(60, 18).addBox(-4.0F, -4.0F, -2.0F, 4.0F, 1.0F, 2.0F, 0.0F, false);
-		Teeth.texOffs(58, 46).addBox(4.0F, -4.0F, -1.0F, 3.0F, 1.0F, 1.0F, 0.0F, false);
-		Teeth.texOffs(60, 54).addBox(-7.0F, -4.0F, -1.0F, 3.0F, 1.0F, 1.0F, 0.0F, false);
-		Teeth.texOffs(44, 54).addBox(0.0F, -4.0F, 1.0F, 7.0F, 1.0F, 1.0F, 0.0F, false);
-		Teeth.texOffs(28, 54).addBox(-7.0F, -4.0F, 1.0F, 7.0F, 1.0F, 1.0F, 0.0F, false);
+		
+
+		topRow = new ModelRenderer(this);
+		topRow.setPos(0.0F, 0.0F, 0.0F);
+		Teeth.addChild(topRow);
+		topRow.texOffs(12, 59).addBox(0.0F, -8.0F, -2.0F, 4.0F, 1.0F, 2.0F, 0.0F, false);
+		topRow.texOffs(60, 18).addBox(-4.0F, -8.0F, -2.0F, 4.0F, 1.0F, 2.0F, 0.0F, false);
+		topRow.texOffs(58, 46).addBox(4.0F, -8.0F, -1.0F, 3.0F, 1.0F, 1.0F, 0.0F, false);
+		topRow.texOffs(60, 54).addBox(-7.0F, -8.0F, -1.0F, 3.0F, 1.0F, 1.0F, 0.0F, false);
+
+		btmRow = new ModelRenderer(this);
+		btmRow.setPos(0.0F, 0.0F, 0.0F);
+		Teeth.addChild(btmRow);
+		btmRow.texOffs(44, 54).addBox(0.0F, -8.0F, 1.0F, 7.0F, 1.0F, 1.0F, 0.0F, false);
+		btmRow.texOffs(28, 54).addBox(-7.0F, -8.0F, 1.0F, 7.0F, 1.0F, 1.0F, 0.0F, false);
 
 		Base = new ModelRenderer(this);
 		Base.setPos(0.0F, 24.0F, 0.0F);
 		setRotationAngle(Base, 0.0F, 3.1416F, 0.0F);
-		Base.texOffs(0, 0).addBox(-13.0F, -2.0F, -8.0F, 26.0F, 2.0F, 16.0F, 0.0F, false);
+		Base.texOffs(0, 0).addBox(-13.0F, -6.0F, -8.0F, 26.0F, 2.0F, 16.0F, 0.0F, false);
 
-		Tentacles = new ModelRenderer(this);
-		Tentacles.setPos(0.0F, 0.0F, 0.0F);
-		Base.addChild(Tentacles);
+		TopTentacle = new ModelRenderer(this);
+		TopTentacle.setPos(0.0F, 24.0F, 0.0F);
 		
 
-		TopL_Tentacle = new ModelRenderer(this);
-		TopL_Tentacle.setPos(0.0F, 0.0F, 0.0F);
-		Tentacles.addChild(TopL_Tentacle);
-		TopL_Tentacle.texOffs(40, 41).addBox(4.0F, -2.0F, -17.0F, 4.0F, 2.0F, 5.0F, 0.0F, false);
-		TopL_Tentacle.texOffs(40, 56).addBox(5.0F, -2.0F, -21.0F, 2.0F, 2.0F, 4.0F, 0.0F, false);
+		baseTT = new ModelRenderer(this);
+		baseTT.setPos(-2.0F, 0.0F, 9.0F);
+		TopTentacle.addChild(baseTT);
+		
 
 		cube_r1 = new ModelRenderer(this);
-		cube_r1.setPos(4.0F, 0.0F, -9.0F);
-		TopL_Tentacle.addChild(cube_r1);
+		cube_r1.setPos(0.0F, 3.0F, 0.0F);
+		baseTT.addChild(cube_r1);
 		setRotationAngle(cube_r1, 0.0F, -1.5708F, 0.0F);
-		cube_r1.texOffs(20, 34).addBox(-3.0F, -2.0F, -5.0F, 4.0F, 2.0F, 6.0F, 0.0F, false);
+		cube_r1.texOffs(40, 26).addBox(-1.0F, -9.0F, -5.0F, 4.0F, 2.0F, 6.0F, 0.0F, false);
 
-		TopR_Tentacle = new ModelRenderer(this);
-		TopR_Tentacle.setPos(0.0F, 0.0F, 0.0F);
-		Tentacles.addChild(TopR_Tentacle);
-		TopR_Tentacle.texOffs(40, 34).addBox(-9.0F, -2.0F, -17.0F, 4.0F, 2.0F, 5.0F, 0.0F, false);
-		TopR_Tentacle.texOffs(28, 56).addBox(-8.0F, -2.0F, -21.0F, 2.0F, 2.0F, 4.0F, 0.0F, false);
+		middleTT = new ModelRenderer(this);
+		middleTT.setPos(0.0F, 0.0F, 0.0F);
+		TopTentacle.addChild(middleTT);
+		middleTT.texOffs(42, 18).addBox(-2.0F, -6.0F, 12.0F, 4.0F, 2.0F, 5.0F, 0.0F, false);
+
+		tipTT = new ModelRenderer(this);
+		tipTT.setPos(0.0F, 0.0F, 0.0F);
+		TopTentacle.addChild(tipTT);
+		tipTT.texOffs(54, 48).addBox(-1.0F, -6.0F, 17.0F, 2.0F, 2.0F, 4.0F, 0.0F, false);
+
+		Btm_L_Tentacle = new ModelRenderer(this);
+		Btm_L_Tentacle.setPos(0.0F, 24.0F, 0.0F);
+		
+
+		baseBLT = new ModelRenderer(this);
+		baseBLT.setPos(0.0F, 0.0F, 0.0F);
+		Btm_L_Tentacle.addChild(baseBLT);
+		
 
 		cube_r2 = new ModelRenderer(this);
-		cube_r2.setPos(-9.0F, 0.0F, -9.0F);
-		TopR_Tentacle.addChild(cube_r2);
+		cube_r2.setPos(-9.0F, 3.0F, -9.0F);
+		baseBLT.addChild(cube_r2);
 		setRotationAngle(cube_r2, 0.0F, -1.5708F, 0.0F);
-		cube_r2.texOffs(0, 40).addBox(-3.0F, -2.0F, -5.0F, 4.0F, 2.0F, 6.0F, 0.0F, false);
+		cube_r2.texOffs(0, 40).addBox(-3.0F, -9.0F, -5.0F, 4.0F, 2.0F, 6.0F, 0.0F, false);
 
-		LeftT_Tentacle = new ModelRenderer(this);
-		LeftT_Tentacle.setPos(0.0F, 0.0F, 0.0F);
-		Tentacles.addChild(LeftT_Tentacle);
-		LeftT_Tentacle.texOffs(0, 24).addBox(-17.0F, -2.0F, -7.0F, 4.0F, 2.0F, 6.0F, 0.0F, false);
-		LeftT_Tentacle.texOffs(0, 48).addBox(-22.0F, -2.0F, -6.0F, 5.0F, 2.0F, 4.0F, 0.0F, false);
+		middleBLT = new ModelRenderer(this);
+		middleBLT.setPos(0.0F, 0.0F, 0.0F);
+		Btm_L_Tentacle.addChild(middleBLT);
+		middleBLT.texOffs(40, 34).addBox(-9.0F, -6.0F, -17.0F, 4.0F, 2.0F, 5.0F, 0.0F, false);
+
+		tipBLT = new ModelRenderer(this);
+		tipBLT.setPos(0.0F, 0.0F, 0.0F);
+		Btm_L_Tentacle.addChild(tipBLT);
+		tipBLT.texOffs(28, 56).addBox(-8.0F, -6.0F, -21.0F, 2.0F, 2.0F, 4.0F, 0.0F, false);
+
+		Btm_R_Tentacle = new ModelRenderer(this);
+		Btm_R_Tentacle.setPos(0.0F, 24.0F, 0.0F);
+		
+
+		baseBRT = new ModelRenderer(this);
+		baseBRT.setPos(0.0F, 0.0F, 0.0F);
+		Btm_R_Tentacle.addChild(baseBRT);
+		
 
 		cube_r3 = new ModelRenderer(this);
-		cube_r3.setPos(-23.0F, 0.0F, -4.0F);
-		LeftT_Tentacle.addChild(cube_r3);
-		setRotationAngle(cube_r3, 0.0F, 1.5708F, 0.0F);
-		cube_r3.texOffs(0, 59).addBox(-1.0F, -2.0F, -3.0F, 2.0F, 2.0F, 4.0F, 0.0F, false);
+		cube_r3.setPos(4.0F, 3.0F, -9.0F);
+		baseBRT.addChild(cube_r3);
+		setRotationAngle(cube_r3, 0.0F, -1.5708F, 0.0F);
+		cube_r3.texOffs(20, 34).addBox(-3.0F, -9.0F, -5.0F, 4.0F, 2.0F, 6.0F, 0.0F, false);
 
-		LeftB_Tentacle = new ModelRenderer(this);
-		LeftB_Tentacle.setPos(0.0F, 0.0F, 0.0F);
-		Tentacles.addChild(LeftB_Tentacle);
-		LeftB_Tentacle.texOffs(22, 18).addBox(-17.0F, -2.0F, 1.0F, 4.0F, 2.0F, 6.0F, 0.0F, false);
-		LeftB_Tentacle.texOffs(20, 42).addBox(-22.0F, -2.0F, 2.0F, 5.0F, 2.0F, 4.0F, 0.0F, false);
+		middleBRT = new ModelRenderer(this);
+		middleBRT.setPos(0.0F, 0.0F, 0.0F);
+		Btm_R_Tentacle.addChild(middleBRT);
+		middleBRT.texOffs(40, 41).addBox(4.0F, -6.0F, -17.0F, 4.0F, 2.0F, 5.0F, 0.0F, false);
+
+		tipBRT = new ModelRenderer(this);
+		tipBRT.setPos(0.0F, 0.0F, 0.0F);
+		Btm_R_Tentacle.addChild(tipBRT);
+		tipBRT.texOffs(40, 56).addBox(5.0F, -6.0F, -21.0F, 2.0F, 2.0F, 4.0F, 0.0F, false);
+
+		L_Top_Tentacle = new ModelRenderer(this);
+		L_Top_Tentacle.setPos(0.0F, 24.0F, 0.0F);
+		
+
+		baseLTT = new ModelRenderer(this);
+		baseLTT.setPos(0.0F, 0.0F, 0.0F);
+		L_Top_Tentacle.addChild(baseLTT);
+		baseLTT.texOffs(22, 18).addBox(-17.0F, -6.0F, 1.0F, 4.0F, 2.0F, 6.0F, 0.0F, false);
+
+		middleLTT = new ModelRenderer(this);
+		middleLTT.setPos(0.0F, 0.0F, 0.0F);
+		L_Top_Tentacle.addChild(middleLTT);
+		middleLTT.texOffs(20, 42).addBox(-22.0F, -6.0F, 2.0F, 5.0F, 2.0F, 4.0F, 0.0F, false);
+
+		tipLTT = new ModelRenderer(this);
+		tipLTT.setPos(0.0F, 0.0F, 0.0F);
+		L_Top_Tentacle.addChild(tipLTT);
+		
 
 		cube_r4 = new ModelRenderer(this);
-		cube_r4.setPos(-23.0F, 0.0F, 4.0F);
-		LeftB_Tentacle.addChild(cube_r4);
+		cube_r4.setPos(-23.0F, 3.0F, 4.0F);
+		tipLTT.addChild(cube_r4);
 		setRotationAngle(cube_r4, 0.0F, 1.5708F, 0.0F);
-		cube_r4.texOffs(58, 40).addBox(-1.0F, -2.0F, -3.0F, 2.0F, 2.0F, 4.0F, 0.0F, false);
+		cube_r4.texOffs(58, 40).addBox(-1.0F, -9.0F, -3.0F, 2.0F, 2.0F, 4.0F, 0.0F, false);
 
-		RightT_Tentacle = new ModelRenderer(this);
-		RightT_Tentacle.setPos(0.0F, 0.0F, 0.0F);
-		Tentacles.addChild(RightT_Tentacle);
-		RightT_Tentacle.texOffs(20, 26).addBox(13.0F, -2.0F, -7.0F, 4.0F, 2.0F, 6.0F, 0.0F, false);
-		RightT_Tentacle.texOffs(18, 48).addBox(17.0F, -2.0F, -6.0F, 5.0F, 2.0F, 4.0F, 0.0F, false);
+		L_Btm_Tentacle = new ModelRenderer(this);
+		L_Btm_Tentacle.setPos(0.0F, 24.0F, 0.0F);
+		
+
+		baseLBT = new ModelRenderer(this);
+		baseLBT.setPos(0.0F, 0.0F, 0.0F);
+		L_Btm_Tentacle.addChild(baseLBT);
+		baseLBT.texOffs(0, 24).addBox(-17.0F, -6.0F, -7.0F, 4.0F, 2.0F, 6.0F, 0.0F, false);
+
+		middleLBT = new ModelRenderer(this);
+		middleLBT.setPos(0.0F, 0.0F, 0.0F);
+		L_Btm_Tentacle.addChild(middleLBT);
+		middleLBT.texOffs(0, 48).addBox(-22.0F, -6.0F, -6.0F, 5.0F, 2.0F, 4.0F, 0.0F, false);
+
+		tipLBT = new ModelRenderer(this);
+		tipLBT.setPos(0.0F, 0.0F, 0.0F);
+		L_Btm_Tentacle.addChild(tipLBT);
+		
 
 		cube_r5 = new ModelRenderer(this);
-		cube_r5.setPos(25.0F, 0.0F, -4.0F);
-		RightT_Tentacle.addChild(cube_r5);
+		cube_r5.setPos(-23.0F, 3.0F, -4.0F);
+		tipLBT.addChild(cube_r5);
 		setRotationAngle(cube_r5, 0.0F, 1.5708F, 0.0F);
-		cube_r5.texOffs(52, 56).addBox(-1.0F, -2.0F, -3.0F, 2.0F, 2.0F, 4.0F, 0.0F, false);
+		cube_r5.texOffs(0, 59).addBox(-1.0F, -9.0F, -3.0F, 2.0F, 2.0F, 4.0F, 0.0F, true);
 
-		RightB_Tentacle = new ModelRenderer(this);
-		RightB_Tentacle.setPos(0.0F, 0.0F, 0.0F);
-		Tentacles.addChild(RightB_Tentacle);
-		RightB_Tentacle.texOffs(0, 32).addBox(13.0F, -2.0F, 1.0F, 4.0F, 2.0F, 6.0F, 0.0F, false);
-		RightB_Tentacle.texOffs(36, 48).addBox(17.0F, -2.0F, 2.0F, 5.0F, 2.0F, 4.0F, 0.0F, false);
+		R_Top_Tentacle = new ModelRenderer(this);
+		R_Top_Tentacle.setPos(0.0F, 24.0F, 0.0F);
+		
+
+		baseRTT = new ModelRenderer(this);
+		baseRTT.setPos(0.0F, 0.0F, 0.0F);
+		R_Top_Tentacle.addChild(baseRTT);
+		baseRTT.texOffs(0, 32).addBox(13.0F, -6.0F, 1.0F, 4.0F, 2.0F, 6.0F, 0.0F, false);
+
+		middleRTT = new ModelRenderer(this);
+		middleRTT.setPos(0.0F, 0.0F, 0.0F);
+		R_Top_Tentacle.addChild(middleRTT);
+		middleRTT.texOffs(36, 48).addBox(17.0F, -6.0F, 2.0F, 5.0F, 2.0F, 4.0F, 0.0F, false);
+
+		tipRTT = new ModelRenderer(this);
+		tipRTT.setPos(0.0F, 0.0F, 0.0F);
+		R_Top_Tentacle.addChild(tipRTT);
+		
 
 		cube_r6 = new ModelRenderer(this);
-		cube_r6.setPos(25.0F, 0.0F, 4.0F);
-		RightB_Tentacle.addChild(cube_r6);
+		cube_r6.setPos(25.0F, 3.0F, 4.0F);
+		tipRTT.addChild(cube_r6);
 		setRotationAngle(cube_r6, 0.0F, 1.5708F, 0.0F);
-		cube_r6.texOffs(58, 34).addBox(-1.0F, -2.0F, -3.0F, 2.0F, 2.0F, 4.0F, 0.0F, false);
+		cube_r6.texOffs(58, 34).addBox(-1.0F, -9.0F, -3.0F, 2.0F, 2.0F, 4.0F, 0.0F, false);
 
-		Bottom_Tentacle = new ModelRenderer(this);
-		Bottom_Tentacle.setPos(0.0F, 0.0F, 0.0F);
-		Tentacles.addChild(Bottom_Tentacle);
-		Bottom_Tentacle.texOffs(42, 18).addBox(-2.0F, -2.0F, 12.0F, 4.0F, 2.0F, 5.0F, 0.0F, false);
-		Bottom_Tentacle.texOffs(54, 48).addBox(-1.0F, -2.0F, 17.0F, 2.0F, 2.0F, 4.0F, 0.0F, false);
+		R_Btm_Tentacle = new ModelRenderer(this);
+		R_Btm_Tentacle.setPos(0.0F, 24.0F, 0.0F);
+		
+
+		baseRBT = new ModelRenderer(this);
+		baseRBT.setPos(0.0F, 0.0F, 0.0F);
+		R_Btm_Tentacle.addChild(baseRBT);
+		baseRBT.texOffs(20, 26).addBox(13.0F, -6.0F, -7.0F, 4.0F, 2.0F, 6.0F, 0.0F, false);
+
+		middleRBT = new ModelRenderer(this);
+		middleRBT.setPos(0.0F, 0.0F, 0.0F);
+		R_Btm_Tentacle.addChild(middleRBT);
+		middleRBT.texOffs(18, 48).addBox(17.0F, -6.0F, -6.0F, 5.0F, 2.0F, 4.0F, 0.0F, false);
+
+		tipRBT = new ModelRenderer(this);
+		tipRBT.setPos(0.0F, 0.0F, 0.0F);
+		R_Btm_Tentacle.addChild(tipRBT);
+		
 
 		cube_r7 = new ModelRenderer(this);
-		cube_r7.setPos(-2.0F, 0.0F, 9.0F);
-		Bottom_Tentacle.addChild(cube_r7);
-		setRotationAngle(cube_r7, 0.0F, -1.5708F, 0.0F);
-		cube_r7.texOffs(40, 26).addBox(-1.0F, -2.0F, -5.0F, 4.0F, 2.0F, 6.0F, 0.0F, false);
+		cube_r7.setPos(25.0F, 3.0F, -4.0F);
+		tipRBT.addChild(cube_r7);
+		setRotationAngle(cube_r7, 0.0F, 1.5708F, 0.0F);
+		cube_r7.texOffs(52, 56).addBox(-1.0F, -9.0F, -3.0F, 2.0F, 2.0F, 4.0F, 0.0F, false);
 	}
 
 	@Override
@@ -170,6 +287,13 @@ public class ceiling_mouth extends EntityModel<Entity> {
 	public void renderToBuffer(MatrixStack matrixStack, IVertexBuilder buffer, int packedLight, int packedOverlay, float red, float green, float blue, float alpha){
 		Mouth.render(matrixStack, buffer, packedLight, packedOverlay, red, green, blue, alpha);
 		Base.render(matrixStack, buffer, packedLight, packedOverlay, red, green, blue, alpha);
+		TopTentacle.render(matrixStack, buffer, packedLight, packedOverlay, red, green, blue, alpha);
+		Btm_L_Tentacle.render(matrixStack, buffer, packedLight, packedOverlay, red, green, blue, alpha);
+		Btm_R_Tentacle.render(matrixStack, buffer, packedLight, packedOverlay, red, green, blue, alpha);
+		L_Top_Tentacle.render(matrixStack, buffer, packedLight, packedOverlay, red, green, blue, alpha);
+		L_Btm_Tentacle.render(matrixStack, buffer, packedLight, packedOverlay, red, green, blue, alpha);
+		R_Top_Tentacle.render(matrixStack, buffer, packedLight, packedOverlay, red, green, blue, alpha);
+		R_Btm_Tentacle.render(matrixStack, buffer, packedLight, packedOverlay, red, green, blue, alpha);
 	}
 
 	public void setRotationAngle(ModelRenderer modelRenderer, float x, float y, float z) {
--- Kay_EntityItems/Entity_Exports/the_screamer.java
+++ Kay_EntityItems/Entity_Exports/the_screamer.java
@@ -6,17 +6,34 @@
 public class the_screamer extends EntityModel<Entity> {
 	private final ModelRenderer Head;
 	private final ModelRenderer Torso;
+	private final ModelRenderer shoulders_ribs;
 	private final ModelRenderer LowerBody;
 	private final ModelRenderer Tentacles;
 	private final ModelRenderer Front;
+	private final ModelRenderer front_base;
+	private final ModelRenderer front_middle1;
+	private final ModelRenderer front_middle2;
+	private final ModelRenderer front_tip;
 	private final ModelRenderer Back;
+	private final ModelRenderer back_base;
+	private final ModelRenderer back_middle1;
+	private final ModelRenderer back_middle2;
+	private final ModelRenderer back_tip;
 	private final ModelRenderer Left;
+	private final ModelRenderer left_base;
+	private final ModelRenderer left_middle1;
 	private final ModelRenderer cube_r1;
+	private final ModelRenderer left_middle2;
 	private final ModelRenderer cube_r2;
+	private final ModelRenderer left_tip;
 	private final ModelRenderer cube_r3;
 	private final ModelRenderer Right;
+	private final ModelRenderer right_base;
+	private final ModelRenderer right_middle1;
 	private final ModelRenderer cube_r4;
+	private final ModelRenderer right_middle2;
 	private final ModelRenderer cube_r5;
+	private final ModelRenderer right_tip;
 	private final ModelRenderer cube_r6;
 
 	public the_screamer() {
@@ -29,15 +46,19 @@ public class the_screamer extends EntityModel<Entity> {
 		Head.texOffs(14, 35).addBox(5.0F, -25.0F, -1.0F, 2.0F, 11.0F, 1.0F, 0.0F, false);
 		Head.texOffs(16, 26).addBox(4.0F, -25.0F, -1.0F, 1.0F, 1.0F, 2.0F, 0.0F, false);
 		Head.texOffs(20, 46).addBox(7.0F, -25.0F, -1.0F, 1.0F, 1.0F, 2.0F, 0.0F, false);
+		Head.texOffs(46, 17).addBox(5.0F, -25.0F, 0.0F, 2.0F, 2.0F, 2.0F, 0.0F, false);
 
 		Torso = new ModelRenderer(this);
 		Torso.setPos(-6.0F, 24.0F, -1.0F);
-		Torso.texOffs(46, 17).addBox(5.0F, -25.0F, 0.0F, 2.0F, 2.0F, 2.0F, 0.0F, false);
-		Torso.texOffs(24, 16).addBox(2.0F, -23.0F, 0.0F, 8.0F, 2.0F, 3.0F, 0.0F, false);
-		Torso.texOffs(0, 26).addBox(3.0F, -21.0F, 0.0F, 6.0F, 4.0F, 2.0F, 0.0F, false);
-		Torso.texOffs(46, 27).addBox(4.0F, -17.0F, 0.0F, 1.0F, 1.0F, 2.0F, 0.0F, false);
 		Torso.texOffs(46, 30).addBox(7.0F, -17.0F, 0.0F, 1.0F, 1.0F, 2.0F, 0.0F, false);
 		Torso.texOffs(46, 12).addBox(5.0F, -17.0F, 0.0F, 2.0F, 3.0F, 2.0F, 0.0F, false);
+
+		shoulders_ribs = new ModelRenderer(this);
+		shoulders_ribs.setPos(0.0F, 0.0F, 0.0F);
+		Torso.addChild(shoulders_ribs);
+		shoulders_ribs.texOffs(24, 16).addBox(2.0F, -23.0F, 0.0F, 8.0F, 2.0F, 3.0F, 0.0F, false);
+		shoulders_ribs.texOffs(0, 26).addBox(3.0F, -21.0F, 0.0F, 6.0F, 4.0F, 2.0F, 0.0F, false);
+		shoulders_ribs.texOffs(46, 27).addBox(4.0F, -17.0F, 0.0F, 1.0F, 1.0F, 2.0F, 0.0F, false);
 
 		LowerBody = new ModelRenderer(this);
 		LowerBody.setPos(0.0F, 9.0F, 0.0F);
@@ -52,64 +73,138 @@ public class the_screamer extends EntityModel<Entity> {
 		Front = new ModelRenderer(this);
 		Front.setPos(0.0F, 0.0F, 0.0F);
 		Tentacles.addChild(Front);
-		Front.texOffs(32, 29).addBox(-2.0F, 11.0F, -7.0F, 4.0F, 4.0F, 3.0F, 0.0F, false);
-		Front.texOffs(20, 35).addBox(-1.0F, 12.0F, -10.0F, 2.0F, 3.0F, 3.0F, 0.0F, false);
-		Front.texOffs(40, 36).addBox(-1.0F, 13.0F, -13.0F, 2.0F, 2.0F, 3.0F, 0.0F, false);
-		Front.texOffs(0, 46).addBox(-1.0F, 14.0F, -16.0F, 2.0F, 1.0F, 3.0F, 0.0F, false);
+		
+
+		front_base = new ModelRenderer(this);
+		front_base.setPos(0.0F, 0.0F, 0.0F);
+		Front.addChild(front_base);
+		front_base.texOffs(32, 29).addBox(-2.0F, 11.0F, -7.0F, 4.0F, 4.0F, 3.0F, 0.0F, false);
+
+		front_middle1 = new ModelRenderer(this);
+		front_middle1.setPos(0.0F, 0.0F, 0.0F);
+		Front.addChild(front_middle1);
+		front_middle1.texOffs(20, 35).addBox(-1.0F, 12.0F, -10.0F, 2.0F, 3.0F, 3.0F, 0.0F, false);
+
+		front_middle2 = new ModelRenderer(this);
+		front_middle2.setPos(0.0F, 0.0F, 0.0F);
+		Front.addChild(front_middle2);
+		front_middle2.texOffs(40, 36).addBox(-1.0F, 13.0F, -13.0F, 2.0F, 2.0F, 3.0F, 0.0F, false);
+
+		front_tip = new ModelRenderer(this);
+		front_tip.setPos(0.0F, 0.0F, 0.0F);
+		Front.addChild(front_tip);
+		front_tip.texOffs(0, 46).addBox(-1.0F, 14.0F, -16.0F, 2.0F, 1.0F, 3.0F, 0.0F, false);
 
 		Back = new ModelRenderer(this);
 		Back.setPos(0.0F, 0.0F, 0.0F);
 		Tentacles.addChild(Back);
-		Back.texOffs(32, 8).addBox(-2.0F, 11.0F, 4.0F, 4.0F, 4.0F, 3.0F, 0.0F, false);
-		Back.texOffs(30, 36).addBox(-1.0F, 12.0F, 7.0F, 2.0F, 3.0F, 3.0F, 0.0F, false);
-		Back.texOffs(20, 41).addBox(-1.0F, 13.0F, 10.0F, 2.0F, 2.0F, 3.0F, 0.0F, false);
-		Back.texOffs(46, 0).addBox(-1.0F, 14.0F, 13.0F, 2.0F, 1.0F, 3.0F, 0.0F, false);
+		
+
+		back_base = new ModelRenderer(this);
+		back_base.setPos(0.0F, 0.0F, 0.0F);
+		Back.addChild(back_base);
+		back_base.texOffs(32, 8).addBox(-2.0F, 11.0F, 4.0F, 4.0F, 4.0F, 3.0F, 0.0F, false);
+
+		back_middle1 = new ModelRenderer(this);
+		back_middle1.setPos(0.0F, 0.0F, 0.0F);
+		Back.addChild(back_middle1);
+		back_middle1.texOffs(30, 36).addBox(-1.0F, 12.0F, 7.0F, 2.0F, 3.0F, 3.0F, 0.0F, false);
+
+		back_middle2 = new ModelRenderer(this);
+		back_middle2.setPos(0.0F, 0.0F, 0.0F);
+		Back.addChild(back_middle2);
+		back_middle2.texOffs(20, 41).addBox(-1.0F, 13.0F, 10.0F, 2.0F, 2.0F, 3.0F, 0.0F, false);
+
+		back_tip = new ModelRenderer(this);
+		back_tip.setPos(0.0F, 0.0F, 0.0F);
+		Back.addChild(back_tip);
+		back_tip.texOffs(46, 0).addBox(-1.0F, 14.0F, 13.0F, 2.0F, 1.0F, 3.0F, 0.0F, false);
 
 		Left = new ModelRenderer(this);
 		Left.setPos(0.0F, 0.0F, 0.0F);
 		Tentacles.addChild(Left);
-		Left.texOffs(32, 0).addBox(4.0F, 11.0F, -2.0F, 3.0F, 4.0F, 4.0F, 0.0F, false);
+		
+
+		left_base = new ModelRenderer(this);
+		left_base.setPos(0.0F, 0.0F, 0.0F);
+		Left.addChild(left_base);
+		left_base.texOffs(32, 0).addBox(4.0F, 11.0F, -2.0F, 3.0F, 4.0F, 4.0F, 0.0F, false);
+
+		left_middle1 = new ModelRenderer(this);
+		left_middle1.setPos(0.0F, 0.0F, 0.0F);
+		Left.addChild(left_middle1);
+		
 
 		cube_r1 = new ModelRenderer(this);
-		cube_r1.setPos(14.0F, 15.0F, 0.0F);
-		Left.addChild(cube_r1);
+		cube_r1.setPos(8.0F, 15.0F, 0.0F);
+		left_middle1.addChild(cube_r1);
 		setRotationAngle(cube_r1, 0.0F, -1.5708F, 0.0F);
-		cube_r1.texOffs(46, 4).addBox(-1.0F, -1.0F, -2.0F, 2.0F, 1.0F, 3.0F, 0.0F, false);
+		cube_r1.texOffs(40, 21).addBox(-1.0F, -3.0F, -2.0F, 2.0F, 3.0F, 3.0F, 0.0F, false);
+
+		left_middle2 = new ModelRenderer(this);
+		left_middle2.setPos(0.0F, 0.0F, 0.0F);
+		Left.addChild(left_middle2);
+		
 
 		cube_r2 = new ModelRenderer(this);
 		cube_r2.setPos(11.0F, 15.0F, 0.0F);
-		Left.addChild(cube_r2);
+		left_middle2.addChild(cube_r2);
 		setRotationAngle(cube_r2, 0.0F, -1.5708F, 0.0F);
 		cube_r2.texOffs(30, 42).addBox(-1.0F, -2.0F, -2.0F, 2.0F, 2.0F, 3.0F, 0.0F, false);
 
+		left_tip = new ModelRenderer(this);
+		left_tip.setPos(0.0F, 0.0F, 0.0F);
+		Left.addChild(left_tip);
+		
+
 		cube_r3 = new ModelRenderer(this);
-		cube_r3.setPos(8.0F, 15.0F, 0.0F);
-		Left.addChild(cube_r3);
+		cube_r3.setPos(14.0F, 15.0F, 0.0F);
+		left_tip.addChild(cube_r3);
 		setRotationAngle(cube_r3, 0.0F, -1.5708F, 0.0F);
-		cube_r3.texOffs(40, 21).addBox(-1.0F, -3.0F, -2.0F, 2.0F, 3.0F, 3.0F, 0.0F, false);
+		cube_r3.texOffs(46, 4).addBox(-1.0F, -1.0F, -2.0F, 2.0F, 1.0F, 3.0F, 0.0F, false);
 
 		Right = new ModelRenderer(this);
 		Right.setPos(0.0F, 0.0F, 0.0F);
 		Tentacles.addChild(Right);
-		Right.texOffs(0, 32).addBox(-7.0F, 11.0F, -2.0F, 3.0F, 4.0F, 4.0F, 0.0F, false);
+		
+
+		right_base = new ModelRenderer(this);
+		right_base.setPos(0.0F, 0.0F, 0.0F);
+		Right.addChild(right_base);
+		right_base.texOffs(0, 32).addBox(-7.0F, 11.0F, -2.0F, 3.0F, 4.0F, 4.0F, 0.0F, false);
+
+		right_middle1 = new ModelRenderer(this);
+		right_middle1.setPos(0.0F, 0.0F, 0.0F);
+		Right.addChild(right_middle1);
+		
 
 		cube_r4 = new ModelRenderer(this);
-		cube_r4.setPos(-15.0F, 15.0F, 0.0F);
-		Right.addChild(cube_r4);
+		cube_r4.setPos(-9.0F, 15.0F, 0.0F);
+		right_middle1.addChild(cube_r4);
 		setRotationAngle(cube_r4, 0.0F, -1.5708F, 0.0F);
-		cube_r4.texOffs(46, 8).addBox(-1.0F, -1.0F, -2.0F, 2.0F, 1.0F, 3.0F, 0.0F, false);
+		cube_r4.texOffs(0, 40).addBox(-1.0F, -3.0F, -2.0F, 2.0F, 3.0F, 3.0F, 0.0F, false);
+
+		right_middle2 = new ModelRenderer(this);
+		right_middle2.setPos(0.0F, 0.0F, 0.0F);
+		Right.addChild(right_middle2);
+		
 
 		cube_r5 = new ModelRenderer(this);
 		cube_r5.setPos(-12.0F, 15.0F, 0.0F);
-		Right.addChild(cube_r5);
+		right_middle2.addChild(cube_r5);
 		setRotationAngle(cube_r5, 0.0F, -1.5708F, 0.0F);
 		cube_r5.texOffs(40, 41).addBox(-1.0F, -2.0F, -2.0F, 2.0F, 2.0F, 3.0F, 0.0F, false);
 
+		right_tip = new ModelRenderer(this);
+		right_tip.setPos(0.0F, 0.0F, 0.0F);
+		Right.addChild(right_tip);
+		
+
 		cube_r6 = new ModelRenderer(this);
-		cube_r6.setPos(-9.0F, 15.0F, 0.0F);
-		Right.addChild(cube_r6);
+		cube_r6.setPos(-15.0F, 15.0F, 0.0F);
+		right_tip.addChild(cube_r6);
 		setRotationAngle(cube_r6, 0.0F, -1.5708F, 0.0F);
-		cube_r6.texOffs(0, 40).addBox(-1.0F, -3.0F, -2.0F, 2.0F, 3.0F, 3.0F, 0.0F, false);
+		cube_r6.texOffs(46, 8).addBox(-1.0F, -1.0F, -2.0F, 2.0F, 1.0F, 3.0F, 0.0F, false);
 	}
 
 	@Override
