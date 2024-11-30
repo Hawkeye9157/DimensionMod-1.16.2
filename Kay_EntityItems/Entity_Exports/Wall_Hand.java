1097ee3fcb7080bad90e82ca229d4c8a0de16b31
KayDesist <160885770+KayDesist@users.noreply.github.com>
2024-11-30 01:57:13 -0700

    Animations done!

--- Kay_EntityItems/Entity_Exports/Wall_Hand.java
+++ Kay_EntityItems/Entity_Exports/Wall_Hand.java
@@ -7,13 +7,27 @@ public class Wall_Hand extends EntityModel<Entity> {
 	private final ModelRenderer Forearm;
 	private final ModelRenderer Wrist;
 	private final ModelRenderer palmhand;
+	private final ModelRenderer btm_palm;
+	private final ModelRenderer top_palm;
 	private final ModelRenderer thumb;
+	private final ModelRenderer thumb_base;
 	private final ModelRenderer cube_r1;
+	private final ModelRenderer thumb_middle;
 	private final ModelRenderer cube_r2;
+	private final ModelRenderer thumb_tip;
 	private final ModelRenderer cube_r3;
 	private final ModelRenderer index;
+	private final ModelRenderer index_base;
+	private final ModelRenderer index_middle;
+	private final ModelRenderer index_tip;
 	private final ModelRenderer middle;
+	private final ModelRenderer middle_base;
+	private final ModelRenderer middle_middle;
+	private final ModelRenderer middle_tip;
 	private final ModelRenderer pinky;
+	private final ModelRenderer pinky_base;
+	private final ModelRenderer pinky_middle;
+	private final ModelRenderer pinky_tip;
 
 	public Wall_Hand() {
 		textureWidth = 128;
@@ -31,49 +45,112 @@ public class Wall_Hand extends EntityModel<Entity> {
 
 		palmhand = new ModelRenderer(this);
 		palmhand.setRotationPoint(0.0F, 24.0F, 0.0F);
-		palmhand.setTextureOffset(0, 0).addBox(-7.0F, -21.0F, -3.0F, 16.0F, 4.0F, 7.0F, 0.0F, false);
-		palmhand.setTextureOffset(0, 11).addBox(-8.0F, -27.0F, -2.0F, 14.0F, 6.0F, 6.0F, 0.0F, false);
+		
+
+		btm_palm = new ModelRenderer(this);
+		btm_palm.setRotationPoint(0.0F, 0.0F, 0.0F);
+		palmhand.addChild(btm_palm);
+		btm_palm.setTextureOffset(0, 0).addBox(-7.0F, -21.0F, -3.0F, 16.0F, 4.0F, 7.0F, 0.0F, false);
+
+		top_palm = new ModelRenderer(this);
+		top_palm.setRotationPoint(0.0F, 0.0F, 0.0F);
+		palmhand.addChild(top_palm);
+		top_palm.setTextureOffset(0, 11).addBox(-8.0F, -27.0F, -2.0F, 14.0F, 6.0F, 6.0F, 0.0F, false);
 
 		thumb = new ModelRenderer(this);
 		thumb.setRotationPoint(0.0F, 24.0F, 0.0F);
 		
 
+		thumb_base = new ModelRenderer(this);
+		thumb_base.setRotationPoint(0.0F, 0.0F, 0.0F);
+		thumb.addChild(thumb_base);
+		
+
 		cube_r1 = new ModelRenderer(this);
-		cube_r1.setRotationPoint(11.0F, -28.0F, 0.0F);
-		thumb.addChild(cube_r1);
-		setRotationAngle(cube_r1, 0.0F, 0.0F, 0.3054F);
-		cube_r1.setTextureOffset(60, 10).addBox(-1.0F, -5.0F, -1.0F, 2.0F, 5.0F, 3.0F, 0.0F, false);
+		cube_r1.setRotationPoint(7.0F, -21.0F, 0.0F);
+		thumb_base.addChild(cube_r1);
+		setRotationAngle(cube_r1, 0.0F, 0.0F, 0.4363F);
+		cube_r1.setTextureOffset(28, 41).addBox(-1.0F, -6.0F, -2.0F, 4.0F, 9.0F, 5.0F, 0.0F, false);
+
+		thumb_middle = new ModelRenderer(this);
+		thumb_middle.setRotationPoint(0.0F, 0.0F, 0.0F);
+		thumb.addChild(thumb_middle);
+		
 
 		cube_r2 = new ModelRenderer(this);
 		cube_r2.setRotationPoint(10.0F, -26.0F, 0.0F);
-		thumb.addChild(cube_r2);
+		thumb_middle.addChild(cube_r2);
 		setRotationAngle(cube_r2, 0.0F, 0.0F, 0.3491F);
 		cube_r2.setTextureOffset(40, 24).addBox(-1.0F, -3.0F, -2.0F, 3.0F, 3.0F, 5.0F, 0.0F, false);
 
+		thumb_tip = new ModelRenderer(this);
+		thumb_tip.setRotationPoint(0.0F, 0.0F, 0.0F);
+		thumb.addChild(thumb_tip);
+		
+
 		cube_r3 = new ModelRenderer(this);
-		cube_r3.setRotationPoint(7.0F, -21.0F, 0.0F);
-		thumb.addChild(cube_r3);
-		setRotationAngle(cube_r3, 0.0F, 0.0F, 0.4363F);
-		cube_r3.setTextureOffset(28, 41).addBox(-1.0F, -6.0F, -2.0F, 4.0F, 9.0F, 5.0F, 0.0F, false);
+		cube_r3.setRotationPoint(11.0F, -28.0F, 0.0F);
+		thumb_tip.addChild(cube_r3);
+		setRotationAngle(cube_r3, 0.0F, 0.0F, 0.3054F);
+		cube_r3.setTextureOffset(60, 10).addBox(-1.0F, -5.0F, -1.0F, 2.0F, 5.0F, 3.0F, 0.0F, false);
 
 		index = new ModelRenderer(this);
 		index.setRotationPoint(0.0F, 24.0F, 0.0F);
-		index.setTextureOffset(46, 0).addBox(1.0F, -33.0F, -1.0F, 4.0F, 6.0F, 4.0F, 0.0F, false);
-		index.setTextureOffset(46, 51).addBox(2.0F, -39.0F, -1.0F, 3.0F, 6.0F, 4.0F, 0.0F, false);
-		index.setTextureOffset(16, 43).addBox(3.0F, -45.0F, -1.0F, 2.0F, 6.0F, 4.0F, 0.0F, false);
+		
+
+		index_base = new ModelRenderer(this);
+		index_base.setRotationPoint(0.0F, 0.0F, 0.0F);
+		index.addChild(index_base);
+		index_base.setTextureOffset(46, 0).addBox(1.0F, -33.0F, -1.0F, 4.0F, 6.0F, 4.0F, 0.0F, false);
+
+		index_middle = new ModelRenderer(this);
+		index_middle.setRotationPoint(0.0F, 0.0F, 0.0F);
+		index.addChild(index_middle);
+		index_middle.setTextureOffset(46, 51).addBox(2.0F, -39.0F, -1.0F, 3.0F, 6.0F, 4.0F, 0.0F, false);
+
+		index_tip = new ModelRenderer(this);
+		index_tip.setRotationPoint(0.0F, 0.0F, 0.0F);
+		index.addChild(index_tip);
+		index_tip.setTextureOffset(16, 43).addBox(3.0F, -45.0F, -1.0F, 2.0F, 6.0F, 4.0F, 0.0F, false);
 
 		middle = new ModelRenderer(this);
 		middle.setRotationPoint(0.0F, 24.0F, 0.0F);
-		middle.setTextureOffset(46, 41).addBox(-4.0F, -33.0F, -1.0F, 4.0F, 6.0F, 4.0F, 0.0F, false);
-		middle.setTextureOffset(16, 55).addBox(-4.0F, -39.0F, -1.0F, 3.0F, 6.0F, 4.0F, 0.0F, false);
-		middle.setTextureOffset(0, 56).addBox(-4.0F, -45.0F, -1.0F, 2.0F, 6.0F, 4.0F, 0.0F, false);
+		
+
+		middle_base = new ModelRenderer(this);
+		middle_base.setRotationPoint(0.0F, 0.0F, 0.0F);
+		middle.addChild(middle_base);
+		middle_base.setTextureOffset(46, 41).addBox(-4.0F, -33.0F, -1.0F, 4.0F, 6.0F, 4.0F, 0.0F, false);
+
+		middle_middle = new ModelRenderer(this);
+		middle_middle.setRotationPoint(0.0F, 0.0F, 0.0F);
+		middle.addChild(middle_middle);
+		middle_middle.setTextureOffset(16, 55).addBox(-4.0F, -39.0F, -1.0F, 3.0F, 6.0F, 4.0F, 0.0F, false);
+
+		middle_tip = new ModelRenderer(this);
+		middle_tip.setRotationPoint(0.0F, 0.0F, 0.0F);
+		middle.addChild(middle_tip);
+		middle_tip.setTextureOffset(0, 56).addBox(-4.0F, -45.0F, -1.0F, 2.0F, 6.0F, 4.0F, 0.0F, false);
 
 		pinky = new ModelRenderer(this);
 		pinky.setRotationPoint(6.0F, 24.0F, 0.0F);
 		setRotationAngle(pinky, 0.0F, 0.0F, -0.1309F);
-		pinky.setTextureOffset(0, 43).addBox(-11.0F, -33.0F, -1.0F, 4.0F, 9.0F, 4.0F, 0.0F, false);
-		pinky.setTextureOffset(30, 55).addBox(-11.0F, -39.0F, -1.0F, 3.0F, 6.0F, 4.0F, 0.0F, false);
-		pinky.setTextureOffset(56, 24).addBox(-11.0F, -45.0F, -1.0F, 2.0F, 6.0F, 4.0F, 0.0F, false);
+		
+
+		pinky_base = new ModelRenderer(this);
+		pinky_base.setRotationPoint(0.0F, 0.0F, 0.0F);
+		pinky.addChild(pinky_base);
+		pinky_base.setTextureOffset(0, 43).addBox(-11.0F, -33.0F, -1.0F, 4.0F, 9.0F, 4.0F, 0.0F, false);
+
+		pinky_middle = new ModelRenderer(this);
+		pinky_middle.setRotationPoint(0.0F, 0.0F, 0.0F);
+		pinky.addChild(pinky_middle);
+		pinky_middle.setTextureOffset(30, 55).addBox(-11.0F, -39.0F, -1.0F, 3.0F, 6.0F, 4.0F, 0.0F, false);
+
+		pinky_tip = new ModelRenderer(this);
+		pinky_tip.setRotationPoint(0.0F, 0.0F, 0.0F);
+		pinky.addChild(pinky_tip);
+		pinky_tip.setTextureOffset(56, 24).addBox(-11.0F, -45.0F, -1.0F, 2.0F, 6.0F, 4.0F, 0.0F, false);
 	}
 
 	@Override
